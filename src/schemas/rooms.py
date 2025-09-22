edbe19634833cfa5045d7b31b443037db6c558ac
Grayholm <rodiongrayholm@gmail.com>
2025-09-22 14:55:12 +0300

    Добавил начальные данные в БД

--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -5,7 +5,7 @@ from src.schemas.facilities import Facility
 
 class RoomAddRequest(BaseModel):
     title: str
-    description: str
+    description: str | None = None
     price: int
     quantity: int
     facilities_ids: list[int] | None
@@ -14,7 +14,7 @@ class RoomAddRequest(BaseModel):
 class RoomAdd(BaseModel):
     hotel_id: int
     title: str
-    description: str
+    description: str | None = None
     price: int
     quantity: int
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,54 @@
 import pytest
 
 from src.config import settings
-from src.db import Base, engine_null_pool
+from src.db import Base, engine_null_pool, async_session_maker_null_pool
 from httpx import ASGITransport, AsyncClient
 
 from src.main import app
 from src.models import *
+
+import json
+
+from src.schemas.hotels import Hoteladd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
+
+
+async def add_hotels_from_json(file_path: str, db):
+    """
+    Читает JSON файл с отелями и добавляет их в БД
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        hotels_data = json.load(f)
+
+    for hotel_info in hotels_data:
+        hotel = Hoteladd(
+            title=hotel_info['title'],
+            location=hotel_info['location']
+        )
+        await db.hotels.add(hotel)
+
+    await db.commit()
+
+
+async def add_rooms_from_json(file_path: str, db):
+    """
+    Читает JSON файл с комнатами и добавляет их в БД
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        rooms_data = json.load(f)
+
+    for room_info in rooms_data:
+        room = RoomAdd(
+            hotel_id=room_info['hotel_id'],
+            title=room_info['title'],
+            description=room_info['description'],
+            price=room_info['price'],
+            quantity=room_info['quantity'],
+        )
+        await db.rooms.add(room)
+
+    await db.commit()
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -18,6 +61,11 @@ async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+    async with DBManager(session_factory=async_session_maker_null_pool()) as db:
+        await add_hotels_from_json('tests/mock_hotels.json', db)
+        await add_rooms_from_json('tests/mock_rooms.json', db)
+        await db.commit()
 
 @pytest.fixture(scope='session', autouse=True)
 async def register_user(setup_database):
