633c19a259556655c55c929a9893b1ab802886f7
Grayholm <rodiongrayholm@gmail.com>
2025-09-22 18:29:30 +0300

    Добавил тесты для facilities

---
+++ tests/integrations_tests/facilities/test_api.py
@@ -0,0 +1,19 @@
+from src.schemas.facilities import FacilityAdd
+
+
+async def test_add_facility(ac):
+    response = await ac.post(
+        '/facilities',
+        json={
+            "title": "Wi-Fi"
+        }
+    )
+    print(f'{response.json()=}')
+    assert response.status_code == 200
+
+async def test_get_facilities(ac):
+    response = await ac.get(
+        '/facilities'
+    )
+    print(f'{response.json()=}')
+    assert response.status_code == 200
